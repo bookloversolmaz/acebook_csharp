4f8369da6a4a9121472a30ec75af4629e2f1d45a
Russell Coles <russell.o.coles@gmail.com>
2025-05-13 19:40:37 +0100

    DataFlow diagram and tweaks to DbSetup in Tests

--- backend/Acebook/Models/AcebookDbContext.cs
+++ backend/Acebook/Models/AcebookDbContext.cs
@@ -9,8 +9,6 @@ public class AcebookDbContext : DbContext
     public string? DbPath { get; }
     string? DatabaseUsernameArg = Environment.GetEnvironmentVariable("DATABASE_USERNAME");
 
-      string? DatabaseUserameArg = Environment.GetEnvironmentVariable("DATABASE_USERNAME");
-
     public string? GetDatabaseName() {
       string? DatabaseNameArg = Environment.GetEnvironmentVariable("DATABASE_NAME");
 
@@ -32,7 +30,7 @@ public class AcebookDbContext : DbContext
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseNpgsql(@$"Host=localhost;Username={DatabaseUserameArg};Password=1234;Database=" + GetDatabaseName());
+        => optionsBuilder.UseNpgsql(@$"Host=localhost;Username={DatabaseUsernameArg};Password=1234;Database=" + GetDatabaseName());
     
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
